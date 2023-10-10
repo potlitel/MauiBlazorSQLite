0df77126e285f0532a17218558cdb262a95131c1
potlitel <potlitel@gmail.com>
2023-10-10 15:08:06 -0400

    Finalizando.

--- MauiBlazorSQLite/Data/WeatherForecast.cs
+++ MauiBlazorSQLite/Data/WeatherForecast.cs
@@ -1,13 +1,17 @@
+using SQLite;
+
 namespace MauiBlazorSQLite.Data
 {
     public class WeatherForecast
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
         public DateTime Date { get; set; }
-
         public int TemperatureC { get; set; }
+        public int TemperatureF { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
+        [MaxLength(4000)]
         public string Summary { get; set; }
     }
 }
--- MauiBlazorSQLite/Data/WeatherForecastService.cs
+++ MauiBlazorSQLite/Data/WeatherForecastService.cs
@@ -1,20 +1,59 @@
+using SQLite;
+
 namespace MauiBlazorSQLite.Data
 {
     public class WeatherForecastService
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        private string _dbPath;
+        public string StatusMessage { get; set; }
+        private SQLiteAsyncConnection conn;
 
-        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+        public WeatherForecastService(string dbPath)
         {
-            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = startDate.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            }).ToArray());
+            _dbPath = dbPath;
+        }
+
+        private async Task InitAsync()
+        {
+            // Don't Create database if it exists
+            if (conn != null)
+                return;
+            // Create database and WeatherForecast Table
+            conn = new SQLiteAsyncConnection(_dbPath);
+            await conn.CreateTableAsync<WeatherForecast>();
+        }
+
+        public async Task<List<WeatherForecast>> GetForecastAsync()
+        {
+            await InitAsync();
+            return await conn.Table<WeatherForecast>().ToListAsync();
+        }
+
+        public async Task<WeatherForecast> CreateForecastAsync(
+            WeatherForecast paramWeatherForecast)
+        {
+            // Insert
+            await conn.InsertAsync(paramWeatherForecast);
+            // return the object with the
+            // auto incremented Id populated
+            return paramWeatherForecast;
+        }
+
+        public async Task<WeatherForecast> UpdateForecastAsync(
+            WeatherForecast paramWeatherForecast)
+        {
+            // Update
+            await conn.UpdateAsync(paramWeatherForecast);
+            // Return the updated object
+            return paramWeatherForecast;
+        }
+
+        public async Task<WeatherForecast> DeleteForecastAsync(
+            WeatherForecast paramWeatherForecast)
+        {
+            // Delete
+            await conn.DeleteAsync(paramWeatherForecast);
+            return paramWeatherForecast;
         }
     }
 }
